5e5c00306a7c5fb31ec1d19df12d6c28858f132d
coltenappleby <colten.appleby@gmail.com>
2021-03-21 22:51:58 -0400

    struggling with view error in browser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+coup1 = Coupon.new
+coup1.coupon_code = 'subway'
+coup1.store = "AWAY"
+coup1.save
